5a41ec455284d17c35d2796dacce12321c680733
vglalitha <92884821+vglalitha@users.noreply.github.com>
2021-10-29 08:49:08 +0530

    Add files via upload

---
+++ dropwizard-demo/src/main/java/com/dep/resource/Request.java
@@ -0,0 +1,11 @@
+package com.dep.resource;
+
+public class Request {
+    String message;
+    public  String getMessage(){
+        return message;
+    }
+    public void setMessage(String message){
+        this.message=message;
+    }
+}
