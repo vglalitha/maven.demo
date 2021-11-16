41936d57cf5e84d80caece39c77b370021d62bf5
lalitha <lalitha.vg@khoros.com>
2021-11-16 09:18:49 +0530

    changes in request class

--- dropwizard-demo/src/main/java/com/dep/resource/Request.java
+++ dropwizard-demo/src/main/java/com/dep/resource/Request.java
@@ -1,25 +1,16 @@
 package com.dep.resource;
 
-import com.dep.Services.GetTimelineTweets;
-import com.dep.Services.PostTweet;
-import com.dep.Services.TwitterImplement;
-
-
 public class Request {
     String message;
-    TwitterImplement twitterImplement;
-    public Request(TwitterImplement twitterImplement) {
-        this.twitterImplement=twitterImplement;
-    }
-    public Request()
-    {}
-    public String getMessage() {
 
+    public Request() {
+    }
+
+    public String getMessage() {
         return message;
     }
 
     public void setMessage(String message) {
-
         this.message = message;
     }
 
